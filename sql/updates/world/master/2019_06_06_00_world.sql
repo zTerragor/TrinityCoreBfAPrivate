928a0d00d868e039f5546d75487231b3692d0fbc
zgn <gennady.zamalaev@gmail.com>
2019-06-06 01:55:36 +0300

    DB/Misc: fix quest The Balance of Nature

---
+++ sql/updates/world/master/2019_06_06_00_world.sql
@@ -0,0 +1,7 @@
+DELETE FROM `creature_queststarter` WHERE `id` = 2077 AND `quest` = 28713;
+INSERT INTO `creature_queststarter` VALUES
+(2077, 28713);
+
+DELETE FROM `creature_questender` WHERE `id` = 2077 AND `quest` = 28713;
+INSERT INTO `creature_questender` VALUES
+(2077, 28713);
